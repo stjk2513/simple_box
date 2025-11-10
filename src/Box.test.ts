63dfa24e8c3a3d855acc47c5a59668ac2238e5eb
stjk2513 <stjk2513@gmail.com>
2025-11-09 19:01:19 -0500

    create simple box

--- src/Box.test.ts
+++ src/Box.test.ts
@@ -8,3 +8,22 @@ test("renders a basic box", () => {
 
   expect(box.text()).toBe("Hello");
 });
+
+test("on box click, the box should change color to green", async () => {
+  const wrapper = mount(Box);
+
+  const box = wrapper.get('[data-test="box"]');
+  await box.trigger("click");
+
+  expect(box.classes()).toContain("green");
+});
+
+test("on green box click, the box should return back to clear", async () => {
+  const wrapper = mount(Box);
+
+  const box = wrapper.get('[data-test="box"]');
+  await box.trigger("click");
+  await box.trigger("click");
+
+  expect(box.classes()).not.toContain("green");
+});
--- src/Box.vue
+++ src/Box.vue
@@ -1,9 +1,32 @@
-<template><div data-test="box">Hello</div></template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isGreen = ref(false);
+
+function toggleGreen() {
+  isGreen.value = !isGreen.value;
+}
+</script>
+
+<template>
+  <div :class="{ green: isGreen }" data-test="box" @click="toggleGreen">
+    Hello
+  </div>
+</template>
 
 <style scoped>
+.green {
+  background: green;
+}
+
 div {
   border: 1px solid black;
   height: 300px;
   width: 300px;
+  user-select: none;
+}
+
+div:hover {
+  cursor: pointer;
 }
 </style>
